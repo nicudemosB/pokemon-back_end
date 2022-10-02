7adcf3e873502f8e38f7108b46c248b606a17455
Billy <billyn0213@gmail.com>
2022-10-02 15:16:51 -0400

    reverted secret key

--- pokemon_rest_api/settings.py
+++ pokemon_rest_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = 'django-insecure-j79u$o#8m5270opn6^8(9f0l2iydu)vas3vlhf8pw$5+(!ut)d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
